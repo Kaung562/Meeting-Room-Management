778e0d68caaab977f2939d2bf57b10ab77ee66e2
Kaung Myat Thu <kaungmt61@gmail.com>
2026-02-24 15:24:08 +0630

    Remove backend dist folder from repository

--- backend/dist/controllers/authController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.login = login;
-const userService_1 = require("../services/userService");
-async function login(req, res) {
-    const { username, password } = req.body;
-    const user = await (0, userService_1.verifyLogin)(username ?? '', password ?? '');
-    res.json({ user: { id: user.id, username: user.username, name: user.name, role: user.role } });
-}
--- backend/dist/middlewares/asyncHandler.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-type AsyncRouteHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare function asyncHandler(fn: AsyncRouteHandler): (req: Request, res: Response, next: NextFunction) => void;
-export {};
